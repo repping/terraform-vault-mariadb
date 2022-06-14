20ec4be51e8aed4afbbbc8d735a2d9d3ba76a110
Richard Eppingbroek <r.eppingbroek@outlook.com>
2022-06-14 16:00:09 +0200

    test fix ci

---
+++ examples/default/variables.tf
@@ -0,0 +1,50 @@
+variable "mariadb_addr" {
+  description = "Address of the MariaDB database to connect to."
+  type        = string
+  default     = null
+
+  validation {
+    condition     = length(var.mariadb_addr) != 0
+    error_message = "Please specify the address of the MariaDB RDS instance to connect to."
+  }
+}
+variable "mariadb_username" {
+  description = "Username to use to connect to the MariaDB RDS instance."
+  type        = string
+  default     = null
+
+  validation {
+    condition     = length(var.mariadb_username) != 0
+    error_message = "Please specify the username to connect to MariaDB."
+  }
+}
+variable "mariadb_password" {
+  description = "Password to use to connect to the MariaDB RDS instance."
+  type        = string
+  default     = null
+
+  validation {
+    condition     = length(var.mariadb_password) != 0
+    error_message = "Please specifiy the password to connect to MariaDB."
+  }
+}
+variable "vault_addr" {
+  description = "Address of the Vault instance to connect to."
+  type        = string
+  default     = null
+
+  validation {
+    condition     = length(var.vault_addr) != 0
+    error_message = "Please specify the address of the Vault instance to connect to."
+  }
+}
+variable "vault_token" {
+  description = "Token to use to connect to Vault and configure the dynamic secrets engine."
+  type        = string
+  default     = null
+
+  validation {
+    condition     = length(var.vault_token) != 0
+    error_message = "Please specify a token to use to connect to Vault."
+  }
+}
